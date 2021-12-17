da4e292fbdd97f5e465d712a4d5561bd40fb5ed0
Lê Viết Mẫn <manleviet@gmail.com>
2021-12-17 23:30:31 +0100

    Supports violated information

--- src/main/java/at/tugraz/ist/ase/eval/test/TestCase.java
+++ src/main/java/at/tugraz/ist/ase/eval/test/TestCase.java
@@ -12,7 +12,6 @@ import lombok.*;
 import org.chocosolver.solver.constraints.Constraint;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -27,6 +26,8 @@ public class TestCase implements Cloneable {
     private @NonNull List<Assignment> assignments; // the list of assignments
     private List<Constraint> constraints; // the list of Choco constraints which are translated from this test case
     private List<Constraint> negConstraints; // a list of NEGATIVE Choco constraints
+
+    private boolean isViolated; // represents the violation of this test case with the knowledge base
 
     /**
      * Sets a Choco constraint translated from this test case.
@@ -49,6 +50,8 @@ public class TestCase implements Cloneable {
         }
         negConstraints.add(neg_constraint);
     }
+
+    // TODO: Constraint from ChocoKB
 
     @Override
     public String toString() {
