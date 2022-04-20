d53aafd3ee727b88e17781069a518324138b463e
Lê Viết Mẫn <manleviet@gmail.com>
2022-04-20 20:55:28 +0200

    Update ITestCase

--- src/main/java/at/tugraz/ist/ase/eval/test/ITestCase.java
+++ src/main/java/at/tugraz/ist/ase/eval/test/ITestCase.java
@@ -8,5 +8,13 @@
 
 package at.tugraz.ist.ase.eval.test;
 
+import org.chocosolver.solver.constraints.Constraint;
+
+import java.util.List;
+
 public interface ITestCase {
+    List<Constraint> getChocoConstraints();
+    List<Constraint> getNegChocoConstraints();
+
+    boolean isViolated();
 }
